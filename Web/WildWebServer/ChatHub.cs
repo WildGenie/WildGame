b5734709beb2858801b82ec4c38f8132f1d4df51
Bilgehan Zeki ÖZAYTAÇ <zekiozaytac@hotmail.com.tr>
2019-09-13 01:32:53 +0300

    SignalR Web Sunucusu Hazırlandı, Web Client Test Edildi.

---
+++ Web/WildWebServer/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WildWebServer
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
